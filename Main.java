9d724989e26cb88ee1547642fa0a4ab3e6a6ddbd
Jamilya21_ <kylychbekovaj@gmail.com>
2023-12-01 22:23:55 +0600

    first commit

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello! My name is Jamilya. I want to take part in the challenge and improve my programming skills");
+    }
+}
