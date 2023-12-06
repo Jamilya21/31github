cb3e734459a546cb11b131339e258fd85b17c8f6
Jamilya21_ <kylychbekovaj@gmail.com>
2023-12-06 23:32:25 +0600

    add solved task3

--- Task06.java
+++ Task06.java
@@ -6,25 +6,25 @@ public class Task06{
         //task1
         
         Scanner scanner = new Scanner(System.in);
-        // System.out.println("First employee salary: ");
-        // int salary1 = scanner.nextInt();
-        // System.out.println("Second employee salary: ");
-        // int salary2 = scanner.nextInt();
-        // System.out.println("Third employee salary: ");
-        // int salary3 = scanner.nextInt();
+        System.out.println("First employee salary: ");
+        int salary1 = scanner.nextInt();
+        System.out.println("Second employee salary: ");
+        int salary2 = scanner.nextInt();
+        System.out.println("Third employee salary: ");
+        int salary3 = scanner.nextInt();
 
-        // int highest = Math.max(Math.max(salary1, salary2), salary3);
-        // int lowest = Math.min(Math.min(salary1, salary2), salary3);
-        // int difference = highest - lowest;
+        int highest = Math.max(Math.max(salary1, salary2), salary3);
+        int lowest = Math.min(Math.min(salary1, salary2), salary3);
+        int difference = highest - lowest;
 
-        // DecimalFormat decimalFormat = new DecimalFormat("#,##0.0");
-        // String formattedHighest = decimalFormat.format(highest);
-        // String formattedLowwest = decimalFormat.format(lowest);
-        // String formattedDifference = decimalFormat.format(difference);
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.0");
+        String formattedHighest = decimalFormat.format(highest);
+        String formattedLowwest = decimalFormat.format(lowest);
+        String formattedDifference = decimalFormat.format(difference);
 
-        // System.out.println("Highest paid position: " + formattedHighest + " som");
-        // System.out.println("Lowest paid position: " +  formattedLowwest + " som");
-        // System.out.println("Salary difference: " + formattedDifference +" som");
+        System.out.println("Highest paid position: " + formattedHighest + " som");
+        System.out.println("Lowest paid position: " +  formattedLowwest + " som");
+        System.out.println("Salary difference: " + formattedDifference +" som");
 
         //task2
 
@@ -44,9 +44,15 @@ public class Task06{
             System.out.println("Petya coudn't invite so many guests");
         }       
 
-        ///task3
-
-        
+        //task3
+        System.out.println("Enter your age in days: ");
+        int age = scanner.nextInt();
+        int year = age / 365;
+        int month = (age % 365) / 30;
+        int day = (age % 365) % 30;
+        System.out.println("Years: " + year);
+        System.out.println("Month: " + month);
+        System.out.println("Days: " + day);
         scanner.close();
     }
 }
