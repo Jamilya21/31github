546a1585361aa53ac624ccb578fdd528053cf029
Jamilya21_ <kylychbekovaj@gmail.com>
2023-12-19 22:27:22 +0600

    add getters

--- OOP/Task21/Channel.java
+++ OOP/Task21/Channel.java
@@ -5,4 +5,31 @@ public class Channel {
     private String news;
     private String musicChannel;
 
+    public Channel(String mtv, String discovery, String firstChannel, String news, String musicChannel) {
+        this.mtv = mtv;
+        this.discovery = discovery;
+        this.firstChannel = firstChannel;
+        this.news = news;
+        this.musicChannel = musicChannel;
+    }
+
+    public String getMtv() {
+        return mtv;
+    }
+
+    public String getDiscovery() {
+        return discovery;
+    }
+
+    public String getFirstChannel() {
+        return firstChannel;
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    public String getMusicChannel() {
+        return musicChannel;
+    }
 }
