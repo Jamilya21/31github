cd872e0549668ee5b9d9f9422f7e1f11c1e80c6a
Jamilya21_ <kylychbekovaj@gmail.com>
2023-12-20 23:36:48 +0600

    solved

--- OOP/Task22/Elevator.java
+++ OOP/Task22/Elevator.java
@@ -30,32 +30,32 @@ public class Elevator{
         return floor >= 1 && floor <= maxFloor;
     }
 
-//    public void move(int toFloor){
-//        if (isAllowableFloor(toFloor)){
-//            System.out.println("Elevator is moving..");
-//            int startFloor = currentFloor;
-//            int step = (toFloor > currentFloor) ? 1 : -1;
-//            while (currentFloor != toFloor) {
-//                currentFloor += step;
-//                System.out.println("Floor " + currentFloor + " reached.");
-//            }
-//
-//            System.out.println("Elevator has reached the destination floor " + toFloor);
-//            System.out.println("Floors passed: " + getFloorsPassed(startFloor, toFloor));
-//        } else {
-//            System.out.println("Invalid floor number. Please enter a valid floor.");
-//        }
-//    }
-//
-//    private String getFloorsPassed(int startFloor, int endFloor) {
-//        StringBuilder floorsPassed = new StringBuilder();
-//        int step = (endFloor > startFloor) ? 1 : -1;
-//
-//        for (int floor = startFloor; floor != endFloor; floor += step) {
-//            floorsPassed.append(floor).append(", ");
-//        }
-//        floorsPassed.append(endFloor);
-//
-//        return floorsPassed.toString();
-//    }
+    public void move(int toFloor){
+        if (isAllowableFloor(toFloor)){
+            System.out.println("Elevator is moving..");
+            int startFloor = currentFloor;
+            int step = (toFloor > currentFloor) ? 1 : -1;
+            while (currentFloor != toFloor) {
+                currentFloor += step;
+                System.out.println("Floor " + currentFloor + " reached.");
+            }
+
+            System.out.println("Elevator has reached the destination floor " + toFloor);
+            System.out.println("Floors passed: " + getFloorsPassed(startFloor, toFloor));
+        } else {
+            System.out.println("Invalid floor number. Please enter a valid floor.");
+        }
+    }
+
+    private String getFloorsPassed(int startFloor, int endFloor) {
+        StringBuilder floorsPassed = new StringBuilder();
+        int step = (endFloor > startFloor) ? 1 : -1;
+
+        for (int floor = startFloor; floor != endFloor; floor += step) {
+            floorsPassed.append(floor).append(", ");
+        }
+        floorsPassed.append(endFloor);
+
+        return floorsPassed.toString();
+    }
 }
--- OOP/Task22/Main.java
+++ OOP/Task22/Main.java
@@ -1,5 +1,39 @@
+
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the initial floor of the elevator: ");
+        int initialFloor = scanner.nextInt();
+
+        Elevator elevator = new Elevator(initialFloor, 18, 200);
+
+        while (true) {
+            System.out.print("Enter the destination floor (1-" + elevator.getMaxFloor() + "): ");
+            int toFloor = scanner.nextInt();
+
+            if (elevator.isAllowableFloor(toFloor)) {
+                System.out.print("Enter the weight to transport: ");
+                int weight = scanner.nextInt();
+
+                if (elevator.isAllowableWeight(weight)) {
+                    elevator.move(toFloor);
+                } else {
+                    System.out.println("Invalid weight. Please enter a valid weight.");
+                }
+            } else {
+                System.out.println("Invalid floor number. Please enter a valid floor.");
+            }
+
+            System.out.print("Do you want to continue? (yes/no): ");
+            String continueChoice = scanner.next().toLowerCase();
+            if (!continueChoice.equals("yes")) {
+                System.out.println("Exiting program. Goodbye!");
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
